ea46a66495314404d8f294b35b31c109bcf68a8f
Pranis-s <np03cs4s210147@heraldcollege.edu.np>
2023-04-15 15:29:10 +0545

    Sqflite Dependency

--- lib/child/bottom_screens/add_contacts.dart
+++ lib/child/bottom_screens/add_contacts.dart
@@ -1,3 +1,4 @@
+import 'package:final_try/child/bottom_screens/contacts_page.dart';
 import 'package:final_try/components/PrimaryButton.dart';
 import 'package:flutter/material.dart';
 
@@ -10,12 +11,16 @@ class AddContactsPage extends StatelessWidget {
     return SafeArea(
       //SafeArea ensures that content of widget doesnot overlap with device's status bar, nav bar, other ui elements
       child: Container(
+        padding: EdgeInsets.all(12),
         child: Column(//Column display child widgets verticallly one below other
             children: [
           PrimaryButton(
+              //PrimButton display button with title add trusted contacts
               title: "Add trusted contacts",
-              onPressed:
-                  () {}), //PrimButton display button with title add trusted contacts
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => ContactsPage()));
+              }),
         ]),
       ),
     );
