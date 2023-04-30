dc4e8a41e14b38682e399931dc34b87c7e6c03d3
Pranis-s <np03cs4s210147@heraldcollege.edu.np>
2023-04-30 22:15:00 +0545

    Forgot Password

---
+++ lib/child/forgot_password.dart
@@ -0,0 +1,198 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:final_try/components/SecondaryButton.dart';
+import 'package:final_try/components/custom_textfield.dart';
+import 'package:final_try/child/register_child.dart';
+import 'package:final_try/db/share_pref.dart';
+import 'package:final_try/parent/parent_register_screen.dart';
+import 'package:final_try/utils/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import '../components/PrimaryButton.dart';
+import 'bottom_screens/child_home_screen.dart';
+import '../parent/parent_home_screen.dart';
+
+class ForgotPassword extends StatefulWidget {
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  bool isPasswordShown = true;
+  final _formKey = GlobalKey<FormState>();
+  final _formData = Map<String, Object>();
+  bool isLoading = false;
+
+  _onSubmit() async {
+    _formKey.currentState!.save();
+    try {
+      setState(() {
+        isLoading = true;
+      });
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+              email: _formData['email'].toString(),
+              password: _formData['password'].toString());
+      if (userCredential.user != null) {
+        setState(() {
+          isLoading = false;
+        });
+        FirebaseFirestore.instance
+            .collection("users")
+            .doc(userCredential.user!.uid)
+            .get()
+            .then((value) {
+          if (value['type'] == 'parent') {
+            print(value['type']);
+            MySharedPreference.saveUserType('parent');
+            goTo(context, ParentHomeScreen());
+          } else {
+            MySharedPreference.saveUserType('child');
+
+            goTo(context, HomeScreen());
+          }
+        });
+      }
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+      if (e.code == 'user-not-found') {
+        dialogueBox(context, 'No user found for that email.');
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        dialogueBox(context, 'Wrong password provided for that user.');
+        print('Wrong password provided for that user.');
+      }
+    }
+    print(_formData['email']);
+    print(_formData['password']);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Stack(
+            children: [
+              isLoading
+                  ? progressIndicator(context)
+                  : SingleChildScrollView(
+                      child: Column(
+                        children: [
+                          Container(
+                            height: MediaQuery.of(context).size.height * 0.3,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Text(
+                                  'USER LOGIN',
+                                  style: TextStyle(
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.bold,
+                                    color: primaryColor,
+                                  ),
+                                ),
+                                Image.asset(
+                                  'assets/logo.png',
+                                  height: 100,
+                                  width: 100,
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            height: MediaQuery.of(context).size.height * 0.4,
+                            child: Form(
+                              key: _formKey,
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  CustomTextField(
+                                    hintText: 'Enter Email',
+                                    textInputAction: TextInputAction.next,
+                                    keyboardtype: TextInputType.emailAddress,
+                                    prefix: Icon(Icons.person),
+                                    onsave: (email) {
+                                      _formData['email'] = email ?? "";
+                                    },
+                                    validate: (email) {
+                                      if (email!.isEmpty ||
+                                          email.length < 3 ||
+                                          !email.contains("@")) {
+                                        return 'Enter Correct Email';
+                                      }
+                                      return null;
+                                    },
+                                  ),
+                                  CustomTextField(
+                                    hintText: 'Enter Password',
+                                    isPassword: isPasswordShown,
+                                    prefix: Icon(Icons.vpn_key_rounded),
+                                    onsave: (password) {
+                                      _formData['password'] = password ?? "";
+                                    },
+                                    validate: (password) {
+                                      if (password!.isEmpty ||
+                                          password.length < 7) {
+                                        return 'Enter Correct Password';
+                                      }
+                                      return null;
+                                    },
+                                    suffix: IconButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            isPasswordShown = !isPasswordShown;
+                                          });
+                                        },
+                                        icon: isPasswordShown
+                                            ? Icon(Icons.visibility_off)
+                                            : Icon(Icons.visibility)),
+                                  ),
+                                  PrimaryButton(
+                                      title: 'LOGIN',
+                                      onPressed: () {
+                                        // progressIndicator(context);
+                                        if (_formKey.currentState!.validate()) {
+                                          _onSubmit();
+                                        }
+                                      }),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Container(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Forgot password?",
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                                SecondaryButton(
+                                    title: 'Click Here', onPressed: () {}),
+                              ],
+                            ),
+                          ),
+                          SecondaryButton(
+                              title: 'Register as Child',
+                              onPressed: () {
+                                goTo(context, RegisterChildScreen());
+                              }),
+                          SecondaryButton(
+                              title: 'Register as Parent',
+                              onPressed: () {
+                                goTo(context, RegisterParentScreen());
+                              }),
+                        ],
+                      ),
+                    ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
