b2650091376525909876fccbd877c85ed8eeae55
Pranis-s <np03cs4s210147@heraldcollege.edu.np>
2023-05-04 22:02:02 +0545

    Profile Detail

--- lib/child/bottom_screens/profile_page.dart
+++ lib/child/bottom_screens/profile_page.dart
@@ -1,37 +1,82 @@
 import 'package:final_try/utils/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../child_login_screen.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  final auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
+    final user = auth.currentUser;
+    final name = user?.displayName;
+    final email = user?.email;
+    final phone = user?.phoneNumber;
+
     return Scaffold(
-        body: SafeArea(
-      child: Stack(
-        children: [
-          Padding(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
             padding: const EdgeInsets.all(18.0),
-            child: Align(
-                alignment: Alignment.topCenter,
-                child: Text(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
                   'USER PROFILE',
                   style: TextStyle(
                     fontSize: 40,
                     fontWeight: FontWeight.bold,
                     color: primaryColor,
                   ),
-                )),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Name: ${name ?? 'N/A'}',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: primaryColor,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Email: ${email ?? 'N/A'}',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: primaryColor,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Phone: ${phone ?? 'N/A'}',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: primaryColor,
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () async {
+                    await auth.signOut();
+                    Get.offAll(() => LoginScreen());
+                  },
+                  child: Text(
+                    'Logout',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: primaryColor,
+                  ),
+                ),
+              ],
+            ),
           ),
-          Align(
-              alignment: Alignment.center,
-              child: Icon(
-                Icons.person,
-                size: 100,
-                color: primaryColor,
-              ))
-        ],
+        ),
       ),
-    ));
+    );
   }
 }
